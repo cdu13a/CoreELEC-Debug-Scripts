1892f9350a0f9d34ec752b23f3e59ce3901d83e0
cdu13a <cdu13a@gmail.com>
2019-02-06 13:34:00 -0500

    And so it Begins

---
+++ dispinfo.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+#
+# Collect amlogic display information
+#
+#####################################################
+
+
+OUTPUTFILE="/storage/dispinfo.txt"
+
+printf "CoreELEC display information...\n\n" > $OUTPUTFILE
+
+    printf "------------ /etc/os-release ------------" >> $OUTPUTFILE
+if [ -f /etc/os-release ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /etc/os-release >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /proc/device-tree/le-dt-id ------------" >> $OUTPUTFILE
+if [ -f /proc/device-tree/le-dt-id ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /proc/device-tree/le-dt-id >> $OUTPUTFILE
+    printf "\n" >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/edid ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/edid ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/edid >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/edid_parsing ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/edid_parsing ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/edid_parsing >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/rawedid ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/rawedid ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/rawedid >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/config ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/config ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/config >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/dc_cap ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/dc_cap ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/dc_cap >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/attr ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/attr ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/attr >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/disp_cap ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/disp_cap ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/disp_cap >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/preferred_mode ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/preferred_mode ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/preferred_mode >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/devices/virtual/amhdmitx/amhdmitx0/hdr_cap ------------" >> $OUTPUTFILE
+if [ -f /sys/devices/virtual/amhdmitx/amhdmitx0/hdr_cap ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/devices/virtual/amhdmitx/amhdmitx0/hdr_cap >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /sys/class/display/vinfo ------------" >> $OUTPUTFILE
+if [ -f /sys/class/display/vinfo ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /sys/class/display/vinfo >> $OUTPUTFILE
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ relevant kodi settings ------------" >> $OUTPUTFILE
+if [ -f /storage/.kodi/userdata/guisettings.xml ]; then
+    printf "\n" >> $OUTPUTFILE
+    for tag in "coreelec.amlogic.autocdsw" \
+               "coreelec.amlogic.deinterlacing" \
+               "coreelec.amlogic.noisereduction" \
+               "videoplayer.adjustrefreshrate" \
+               "videoplayer.useamcodec" \
+               "videoplayer.useamcodech264" \
+               "videoplayer.useamcodecmpeg2" \
+               "videoplayer.useamcodecmpeg4" \
+               "videoplayer.usedisplayasclock" \
+               "videoscreen.whitelist" \
+               "lookandfeel.skin" 
+    do
+        printf "$tag: " >> $OUTPUTFILE
+        cat /storage/.kodi/userdata/guisettings.xml |grep \""$tag"\" |grep -o '>.*<' |sed -E 's/[<>]//g' >> $OUTPUTFILE
+    done
+else
+    printf " Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.kodi/userdata/disp_cap ------------" >> $OUTPUTFILE
+if [ -f /storage/.kodi/userdata/disp_cap ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /storage/.kodi/userdata/disp_cap >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.config/autostart.sh ------------" >> $OUTPUTFILE
+if [ -f /storage/.config/autostart.sh ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /storage/.config/autostart.sh >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+
+if [ "$1" = "-l" ]; then                                                                   
+  cat $OUTPUTFILE                                                       
+else                              
+  paste $OUTPUTFILE                                                                
+fi      
---
+++ remoteinfo.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+#
+# Collect remote control configuration information
+#
+#####################################################
+
+
+OUTPUTFILE="/storage/remoteinfo.txt"
+
+printf "CoreELEC remote control information...\n\n" > $OUTPUTFILE
+
+    printf "------------ /etc/os-release ------------" >> $OUTPUTFILE
+if [ -f /etc/os-release ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /etc/os-release >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /proc/device-tree/le-dt-id ------------" >> $OUTPUTFILE
+if [ -f /proc/device-tree/le-dt-id ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /proc/device-tree/le-dt-id >> $OUTPUTFILE
+    printf "\n" >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /proc/device-tree/meson-ir/status ------------" >> $OUTPUTFILE
+if [ -f /proc/device-tree/meson-ir/status ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /proc/device-tree/meson-ir/status >> $OUTPUTFILE
+    printf "\n" >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /proc/device-tree/meson-remote/status ------------" >> $OUTPUTFILE
+if [ -f /proc/device-tree/meson-ir/status ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /proc/device-tree/meson-ir/status >> $OUTPUTFILE
+    printf "\n" >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.config/remote.disable ------------" >> $OUTPUTFILE
+if [ -f /storage/.config/remote.disable ]; then
+    printf " Set by user!\n" >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /flash/remote.disable ------------" >> $OUTPUTFILE
+if [ -f /flash/remote.disable ]; then
+    printf " Set by user!\n" >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.config/remote.conf ------------" >> $OUTPUTFILE
+if [ -f /storage/.config/remote.conf ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /storage/.config/remote.conf >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /flash/remote.conf ------------" >> $OUTPUTFILE
+if [ -f /flash/remote.conf ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /flash/remote.conf >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.config/lircd.conf ------------" >> $OUTPUTFILE
+if [ -f /storage/.config/lircd.conf ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /storage/.config/lircd.conf >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.config/lirc_options.conf ------------" >> $OUTPUTFILE
+if [ -f /storage/.config/lirc_options.conf ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /storage/.config/lirc_options.conf >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.config/rc_maps.cfg ------------" >> $OUTPUTFILE
+if [ -f /storage/.config/rc_maps.cfg ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /storage/.config/rc_maps.cfg >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.config/rc_keymaps/* ------------" >> $OUTPUTFILE
+if [ -d /storage/.config/rc_keymaps ]; then
+    printf "\n" >> $OUTPUTFILE
+    for filename in /storage/.config/rc_keymaps/*; do
+        [ -e "$filename" ] || continue
+        printf "------------ $filename ------------" >> $OUTPUTFILE
+        if [ -f $filename ]; then
+            printf "\n" >> $OUTPUTFILE
+            cat $filename >> $OUTPUTFILE
+        else
+            printf " is Directory!\n" >> $OUTPUTFILE
+        fi
+    done
+else
+    printf " Directory Missing!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.kodi/userdata/Lircmap.xml ------------" >> $OUTPUTFILE
+if [ -f /storage/.kodi/userdata/Lircmap.xml ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /storage/.kodi/userdata/Lircmap.xml >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.kodi/userdata/keyboard.xml ------------" >> $OUTPUTFILE
+if [ -f /storage/.kodi/userdata/keyboard.xml ]; then
+    printf "\n" >> $OUTPUTFILE
+    cat /storage/.kodi/userdata/keyboard.xml >> $OUTPUTFILE
+else
+    printf " Unset by user!\n" >> $OUTPUTFILE
+fi
+    printf "------------ /storage/.kodi/userdata/keymaps/*.xml ------------" >> $OUTPUTFILE
+if [ -d /storage/.kodi/userdata/keymaps ]; then
+    printf "\n" >> $OUTPUTFILE
+    for filename in /storage/.kodi/userdata/keymaps/*.xml; do
+        [ -e "$filename" ] || continue
+        printf "------------ $filename ------------" >> $OUTPUTFILE
+        if [ -f $filename ]; then
+            printf "\n" >> $OUTPUTFILE
+            cat $filename >> $OUTPUTFILE
+        else
+            printf " is Directory!\n" >> $OUTPUTFILE
+        fi
+    done
+else
+    printf " Directory Missing!\n" >> $OUTPUTFILE
+fi
+
+if [ "$1" = "-l" ]; then                                                                   
+  cat $OUTPUTFILE                                                       
+else                              
+  paste $OUTPUTFILE                                                                
+fi      
